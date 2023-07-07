f2d0fcedd1687ce4d1fee7a7434912720d79f48c
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-07 09:44:41 +0200

    3rd test passed

--- src/handsUp.js
+++ src/handsUp.js
@@ -1,9 +1,32 @@
 function handsUp(num) {
-if (num === 6 ){
-  return [0, 2, 0];
+
+raisedHands = [0, 0, 0];
+
+for (let i = 0; i < num; i++){
+  if (raisedHands[0] === 2){
+    raisedHands[0] = 0;
+    if (raisedHands[1] === 2){
+      raisedHands[1] = 0
+      if(raisedHands[2] === 2){
+        raisedHands[2] = 0;
+      } else {
+        raisedHands[2] += 1;
+      }
+    } else {
+      raisedHands[1] += 1;
+    }
+    console.log('After first number reset: ', raisedHands)
+
+  } else {
+    raisedHands[0] += 1;
+    console.log('after increment: ', raisedHands)
+  }
 }
 
-  return  [0, 0, 0];
+console.log('final raised: ', raisedHands)
+
+ return raisedHands
+
 }
 
 module.exports = handsUp;
--- src/handsUp.test.js
+++ src/handsUp.test.js
@@ -8,4 +8,8 @@ describe('handsUp', () => {
   it('6 should return [0, 2, 0]', () => {
     expect(handsUp(6)).toStrictEqual([0, 2, 0]);
   });
+
+  it('2 should return [2, 0, 0]', () => {
+    expect(handsUp(2)).toStrictEqual([2, 0, 0]);
+  });
 });
